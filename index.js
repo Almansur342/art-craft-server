f2913ad07614e02e66492141dfe9527f33f667e9
Almansur342 <abdullahalmansur2002@gmail.com>
2024-04-29 20:14:36 +0600

    data get from database by email

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skihu85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,18 +36,28 @@ async function run() {
       const cursor = productCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
+
     app.get('/sixCraftItem', async(req,res)=>{
       const cursor = productCollection.find().limit(6);
       const result = await cursor.toArray();
       res.send(result);
     })
 
-
     app.post('/addProduct', async(req,res)=>{
       const result = await productCollection.insertOne(req.body);
       console.log(result)
       res.send(result);
+    });
+
+    app.get('/myCraftProduct/:email', async(req,res)=>{
+      console.log(req.params.email)
+      // console.log(email);
+      const result = await productCollection.find({email:req.params.email}).toArray();
+      console.log(result);
+      res.send(result);
+   
+
     })
 
     // Send a ping to confirm a successful connection
